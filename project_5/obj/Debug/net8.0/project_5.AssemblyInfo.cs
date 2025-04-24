e4d32609e3045b2fdb617337164601e7dcc35db7
Alexandra Perrin <alexandrarose1@live.com.au>
2025-04-24 11:21:16 +0200

    formatting buttons and check language

--- project_5/obj/Debug/net8.0/project_5.AssemblyInfo.cs
+++ project_5/obj/Debug/net8.0/project_5.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89b6b25704bd536ca76d1fa2157d45e809cc5aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9eb64edbb45b00c0c1df1cd5e0a13a334dbd56a")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
